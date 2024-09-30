5da6f911284ecd7f5d6b6e4429e7d5235ffd6425
Alexander <protonull@protonmail.com>
2024-09-30 09:56:12 +0100

    Just nuke the InteractionTrackerImpl class

--- src/main/java/uk/protonull/civ/chesttracker/utilities/VoidEvents.java
+++ src/main/java/uk/protonull/civ/chesttracker/utilities/VoidEvents.java
@@ -1,14 +1,12 @@
 package uk.protonull.civ.chesttracker.utilities;
 
 import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import org.jetbrains.annotations.NotNull;
 import red.jackf.chesttracker.impl.events.AfterPlayerDestroyBlock;
 import red.jackf.chesttracker.impl.events.AfterPlayerPlaceBlock;
 
 @SuppressWarnings({"UnstableApiUsage", "NonExtendableApiUsage"})
 public final class VoidEvents {
-    public static final Event<UseBlockCallback> USE_BLOCK_CALLBACK = new VoidEvent<>();
     public static final Event<AfterPlayerPlaceBlock> AFTER_PLAYER_PLACE_BLOCK = new VoidEvent<>();
     public static final Event<AfterPlayerDestroyBlock> AFTER_PLAYER_DESTROY_BLOCK = new VoidEvent<>();
 
