4a1b5ea681370b1f7f8f360130b75ab8080b5156
Alexander <protonull@protonmail.com>
2024-11-19 06:28:06 +0000

    Use shadowed level reference instead of getting it through Minecraft
    Also adds a null-check, just in case.

--- src/main/java/uk/protonull/civ/chesttracker/mixins/InterceptCtiMixin.java
+++ src/main/java/uk/protonull/civ/chesttracker/mixins/InterceptCtiMixin.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -14,6 +14,7 @@ import net.minecraft.network.chat.Style;
 import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,6 +27,9 @@ public abstract class InterceptCtiMixin {
     @Unique
     private static final Pattern LOCATION_PATTERN = Pattern.compile("^Location: (-?\\d+) (-?\\d+) (-?\\d+)$");
 
+    @Shadow
+    private ClientLevel level;
+
     /**
      * This works because Citadel sends the reinforcement message before the inventory is opened.
      */
@@ -37,6 +41,9 @@ public abstract class InterceptCtiMixin {
         final @NotNull ClientboundSystemChatPacket packet,
         final @NotNull CallbackInfo ci
     ) {
+        if (this.level == null) {
+            return;
+        }
         final String plainMessage = packet.content().getString().replaceAll("§.", "");
         if (!plainMessage.equals("Not reinforced") && !plainMessage.startsWith("Reinforced at ")) {
             return;
@@ -57,7 +64,7 @@ public abstract class InterceptCtiMixin {
             return;
         }
         InteractionTrackerImpl.INSTANCE.setLastBlockSource(new CachedClientBlockSource(
-            Minecraft.getInstance().level,
+            this.level,
             blockPos
         ));
     }
